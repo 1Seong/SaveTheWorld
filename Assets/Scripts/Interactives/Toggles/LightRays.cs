a7f148c52ffbc91118e929c4c00bc343e4f9566c
SEONG WON CHOI <qwik0102@gmail.com>
2025-12-04 20:55:31 +0900

    [feat] : C면 재작업, 기타 수정

--- Assets/Scripts/Interactives/Toggles/LightRays.cs
+++ Assets/Scripts/Interactives/Toggles/LightRays.cs
@@ -5,6 +5,7 @@ using UnityEngine.Rendering;
 public class LightRays : MonoBehaviour
 {
     public Material targetMaterial;
+    public GameObject hint;
 
     private void Awake()
     {
@@ -13,12 +14,14 @@ public class LightRays : MonoBehaviour
 
     public void LightAppear()
     {
-        targetMaterial.DOFade(0.3f, 0.8f);
+        targetMaterial.DOFade(0.2f, 0.8f);
+        hint.SetActive(true);
     }
 
     public void LightDisappear()
     {
         targetMaterial.DOFade(0f, 0.8f);
+        hint.SetActive(false);
     }
        
 }
